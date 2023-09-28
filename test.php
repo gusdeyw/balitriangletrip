ed0cb1e46828b26c8cb792146952ff8099127802
Gusde Widnyana <widnyana.bagus@gmail.com>
2023-09-28 10:56:30 +0800

    Push Tailwind

---
+++ test.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once "import/import.php"; ?>
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="w-full h-screen bg-cover bg-center pt-52" style="background-image: url(public/images/comingsoonj.jpg);">
+        <div class="px-[125px] grid grid-cols-2 items-center">
+            <div class="mr-5">
+                <h1 class="font-extrabold text-[64px] text-white mb-4">Bali Triangle Trip</h1>
+                <p class="text-white mb-7 text-xl text-justify">Welcome to the Bali Triangle Trip, your ultimate journey into the heart of Bali's enchanting paradise. Immerse yourself in a world where natural beauty, vibrant culture, and thrilling activities converge to create memories that will last a lifetime.</p>
+                <button class="bg-[#1A7472] font-bold w-[146px] h-[45px] text-white">Book Now</button>
+            </div>
+            <div class="flex justify-center ml-[130px]">
+                <div class="h-[355px] w-[258px] bg-cover bg-center" style="background-image: url(public/images/pexels-oleksandr-p-2166559.jpg);">
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="w-full h-screen bg-green-500"></div>
+</body>
+
+</html>
+
+<!-- h-[355px] w-[258px] -->
